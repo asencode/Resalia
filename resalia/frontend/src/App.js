483f0a2dcb6a105513f19839a60dbc9bbe4d4d11
Angel Castillo <angelcastillo410@gmail.com>
2022-09-14 19:01:31 +0200

    V0.2. Inserción de datos y muestra por pantalla.

--- resalia/frontend/src/App.js
+++ resalia/frontend/src/App.js
@@ -1,10 +1,34 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Resalia</a>
       </header>
-      <main></main>
+      <main>
+        <h1>Menú A La Carta</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="productInfo">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <i>{product.category}</i>
+                </p>
+                <p>
+                  <strong>{product.price}€</strong>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
